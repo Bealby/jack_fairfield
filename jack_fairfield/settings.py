27f32576c739aa2376380f28470d513315c0a32c
Bealby <mgbealby@gmail.com>
2023-03-28 21:08:58 +0200

    Set-up allauth

--- jack_fairfield/settings.py
+++ jack_fairfield/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,26 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # Allows auth using either username or emails
+ACCOUNT_EMAIL_REQUIRED = True  # Requries email to register for the site. Mandatory to ensure users use real email.
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Requries email to register for the site. Mandatory to ensure users use real email.
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # Requries email to register for the site. Mandatory to ensure users use real email.
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'jack_fairfield.wsgi.application'
 
